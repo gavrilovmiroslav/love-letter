a1211820013381be109334cdcfc194316f698280
Miroslav <gavrilov.miroslav@gmail.com>
2018-10-17 16:08:04 -0700

    Update rules.scala

--- src/main/scala/rules.scala
+++ src/main/scala/rules.scala
@@ -1,89 +1,4 @@
 
-/*
-
-	Cards:
-
-	5x Guard
-	2x Priest
-	2x Baron
-	2x Handmaid
-	2x Prince
-	1x King
-	1x Countess
-	1x Princess
-
-	Rules:
-
-	Shuffle the 16 cards to form a
-	face-down draw deck. Remove the
-	top card of the deck from the game
-	without looking at it.
-	If you are playing a two-player
-	game, take three more cards from
-	the top of the deck and place them
-	to the side, face up. They will not
-	be used during this round.
-	Each player draws one card from
-	the deck. This is the player’s hand,
-	and is kept secret from the others.
-	Whoever was most recently on a
-	date goes first (if tied, the younger
-	player wins the tie). 
-
-	Love Letter is played in a series
-	of rounds. Each round represents
-	one day. At the end of each round,
-	one player’s letter reaches Princess
-	Annette, and she reads it.
-	When she reads enough letters from
-	one suitor, she becomes enamored
-	and grants that suitor permission
-	to court her. That player wins the
-	princess’s heart and the game. 
-
-	On your turn, draw the top card
-	from the deck and add it to your
-	hand. Then choose one of the two
-	cards in your hand and discard it 
-	face up in front of you. Apply any
-	effect on the card you discarded.
-	You must apply its effect, even if it
-	is bad for you.All discarded cards 
-	remain in front of the player who 
-	discarded them. Overlap the cards so 
-	that it’s clear in which order they 
-	were discarded. This helps players 
-	to figure out which cards other players 
-	might be holding. Once you finish 
-	applying the card’s effect, the turn 
-	passes to the player on your left. 
-
-	If a player is knocked out of the
-	round, that player discards the card
-	in his or her hand face up (do not
-	apply the card’s effect) and takes
-	no more turns until next round. 
-
-	A round ends if the deck is empty
-	at the end of a turn. The royal
-	residence closes for the evening,
-	the person closest to the princess
-	delivers the love letter, and Princess
-	Annette retires to her chambers to
-	read it.
-
-	All players still in the round reveal
-	their hands. The player with the
-	highest ranked person wins the
-	round. In case of a tie, the player
-	who discarded the highest total
-	value of cards wins.
-	A round also ends if all players but
-	one are out of the round, in which
-	case the remaining player wins.
-
-*/
-
 package ll {
 	object util {
 		def next[T](els: Seq[T]): Seq[T] =
@@ -94,7 +9,9 @@ package ll {
 			}
 	}
 
-	package lang {
+	package game {
+		/* entities */
+		
 		sealed trait Entity
 
 		case object Bottom extends Entity
@@ -148,7 +65,7 @@ package ll {
 		object Over {
 			def apply(p: Entity): Over = Over(Option(p))
 		}
-
+		
 		package object cards {
 			case object Guard extends Card { val value = 1 }
 			case object Priest extends Card { val value = 2 }
@@ -167,6 +84,9 @@ package ll {
 					Prince, King, Countess, Princess)
 			}
 		}
+
+		/* continuations (to remember that something else 
+		  		  needs to happen after an action) */
 
 		sealed trait Kont {
 			def ::(k: Kont) = Seq(this, k).toList
@@ -187,6 +107,8 @@ package ll {
 		case class PrinceSelectingPlayerK() extends Kont
 		case class KingSelectingPlayerK(c: Card) extends Kont
 
+		/* states */
+
 		case class State(
 			current: Entity, 
 			players: Seq[Player], 
@@ -206,6 +128,8 @@ package ll {
 
 			def next = this.copy(players = ll.util.next(players))
 		}
+
+		/* interpreter */
 
 		class Interpreter(choiceFns: Map[Player, Choice]) {
 
